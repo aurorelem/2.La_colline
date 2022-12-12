032766c6598554373ffea732ac5bb60b0ff52049
aurorelem <aurorelemaire.pro@gmail.com>
2022-12-12 09:56:20 +0100

    exemples

--- 1.Timeouts-Intervalles/script.js
+++ 1.Timeouts-Intervalles/script.js
@@ -1,1 +1,50 @@
+/*Exemple 1:
+console.log("WARNING! In exactly five seconds something will explode");
+
+function theExplosion() {
+  console.log("BOOM!");
+}
+
+setTimeout(theExplosion, 5000);*/
+
+/*Exemple 2: timeout and events
+function theExplosion() {
+    alert("BOOM!");
+  }
+  
+  const button = document.createElement("button");
+  button.textContent = "WARNING";
+  button.addEventListener("click", () => {
+    // this will delay the event by 5 seconds
+    setTimeout(theExplosion, 5000);
+  });
+  document.body.appendChild(button);*/
+
+  /*Exemple 3: La récursivité                                     A regarder plus tard car n'a pas fct
+  let times = 0;
+  function heyYou() {
+  console.log("Hey! You! I called you " + times + " times!");
+  times++;
+
+  const nextCall = Math.floor(Math.random() * 2000); // Random delay until heyYou() is called again
+  setTimeout(heyYou, nextCall);
+}*/
+
+/*Exemple 3: La setInterval()fonction
+let times = 0;
+function heyYou() {
+  console.log("Hey! You! I called you " + times + " times!");
+  times++;
+}
+
+setInterval(heyYou, 1000);*/
+
+/*Exemple 4: Minuterie d'effacement                              A regarder plus tard car n'a pas fct
+let interval;
+let times = 0;
+
+function heyYou() {
+  console.log("Hey! You! I called you " + times + " times!");
+  times++;
+}*/
 
