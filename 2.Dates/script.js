d966921d2008763b91daf255ab8a0fb3f06c6c70
aurorelem <aurorelemaire.pro@gmail.com>
2022-12-13 16:49:50 +0100

    dernières modifs

--- 2.Dates/script.js
+++ 2.Dates/script.js
@@ -41,3 +41,59 @@ let threeDaysPastHalloween2002 = new Date(
 )
 
 console.log(threeDaysPastHalloween2002.toString())*/
+
+//Exercice 1:
+
+//Ancorage
+/*let anchorage = new Date()
+
+let anchorageHours = anchorage.getHours()
+let anchorageMinutes = anchorage.getMinutes()
+let anchorageSeconds = anchorage.getSeconds()
+let anchorageDate = 'anchorage : ' + anchorageHours + " h " + anchorageMinutes + " min " + anchorageSeconds + "sec"
+
+document.getElementsByClassName("Anchorage")[0].innerHTML = anchorageDate
+console.log(anchorageDate)
+*/
+
+//Reykjavik
+/*let reykjavik = new Date()
+
+let reykjavikHours = reykjavik.getUTCHours()-1
+let reykjavikMinutes = reykjavik.getUTCMinutes()
+let reykjavikSeconds = reykjavik.getUTCSeconds()
+let reykjavikDate = 'Reykjavik : ' + reykjavikHours + " h " + reykjavikMinutes + " min " + reykjavikSeconds + " sec "
+console.log(reykjavikDate)*/
+
+//Saint-Petersbourg
+/*let saint_petersbourg = new Date()
+
+let saint_petersbourgHours = saint_petersbourg.getUTCHours()+3
+let saint_petersbourgMinutes = saint_petersbourg.getUTCMinutes()
+let saint_petersbourgSecond = saint_petersbourg.getUTCSeconds()
+let saint_petersbourgDate = 'saint_petersbourg' + saint_petersbourgHours + " h " + saint_petersbourgMinutes + " min " + saint_petersbourgSecond + " sec "
+document.getElementsByClassName(saint_petersbourg)[0].innerHTML = saint_petersbourgDate
+console.log(saint_petersbourg)*/
+
+//Brussels
+/*let brussels = new Date()
+
+let brusselsHours = brussels.getUTCHours()
+let brusselsMinutes = brussels.getUTCMinutes()
+let brusselsSeconds = brussels.getUTCSeconds()
+let brusselsDate = 'brussels : ' + brusselsHours + " h " + brusselsMinutes + " min " + brusselsSeconds + " sec "
+document.getElementsByClassName("brussels")[0].innerHTML = brusselsDate
+console.log(brusselsDate)*/
+
+//Exercice 2:
+
+/*let date_1 = new Date('10/06/1987');
+let date_2 = new Date();
+
+const days = (date_1, date_2) =>{
+    let difference = date_1.getTime() - date_2.getTime();
+    let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+    return TotalDays;
+}
+console.log(days(date_1, date_2) +" Hé ben, ça en fait des jours!! ");*/
+
--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # 2.La_colline
 
+##1.Timeouts-intervalles
+
 J'ai fais l'exercice 1 au matin et j'ai essayé de faire la taupe en bonus mais j'ai encore du boulot..
 J'ai aussi pris du temps pour combler mon retard en JS et je sais que je reviendrait sur ma taupe..
+
+##2.Dates
+
+J'ai fais l'exercice 1 au matin qui m'a pris du temps et J'ai fais l'exercice deux.
+J'ai continué mon travail sur JS..
