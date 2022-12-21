ab9e29a942631be57ed681b7a252c1c14bc02062
aurorelem <aurorelemaire.pro@gmail.com>
2022-12-21 21:11:59 +0100

    Modif html

--- 2.Weather-app/assets/js/script.js
+++ 2.Weather-app/assets/js/script.js
@@ -7,3 +7,10 @@ const weatherIcons = {
     "Mist": "wi wi-day-fog",
     "drizzle": "wi wi-day-sleet",
  }
+
+
+
+/*Début voir Laura*/ 
+ fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=93470c9d125ec60da741809a54f81589')
+.then(r =>r.json())
+.then(json => console.log(json))
