40b84a725f2a0c3ee3362a7c94e6f004e9e884cf
aurorelem <aurorelemaire.pro@gmail.com>
2022-12-21 12:34:55 +0100

    modifs

--- 2.Weather-app/assets/js/script.js
+++ 2.Weather-app/assets/js/script.js
@@ -1,1 +1,9 @@
-
+//Création d'un objet icones de météo
+const weatherIcons = {
+    "Rain": "wi wi-day-rain",
+    "Clouds": "wi wi-day-cloudy",
+    "Clear": "wi wi-day-cloudy",
+    "Snow": "wi wi-day-snow",
+    "Mist": "wi wi-day-fog",
+    "drizzle": "wi wi-day-sleet",
+ }
